e7c267b616b486144277aeba46f74526a4a0f233
cjahn1305 <152269511+cjahn1305@users.noreply.github.com>
2024-07-29 21:20:48 +0200

    Update290724 main.tf

--- terraform/1_einfache_ec2/main.tf
+++ terraform/1_einfache_ec2/main.tf
@@ -1,13 +1,56 @@
-# Dies ist ein Kommentar
-
-# Provider-Definition (Mindestanforderung für "terraform init")
 provider "aws" {
   region = "eu-central-1"
 }
 
-# Eine Ubuntu EC2 Instanz im kleinen t2.nano-Format
-resource "aws_instance" "erste_instanz" {
-  instance_type = "t2.nano"
-  ami = "ami-01e444924a2233b07"
-  vpc_security_group_ids = [ "sg-09159bd49e5ad69e9", "sg-0f0156a93389ab9cc" ]
+resource "aws_vpc" "main_vpc" {
+  cidr_block = "10.0.0.0/16"
+}
+
+resource "aws_subnet" "private_subnet_a" {
+  vpc_id            = aws_vpc.main_vpc.id
+  cidr_block        = "10.0.1.0/24"
+  availability_zone = "eu-central-1a"
+}
+
+resource "aws_subnet" "private_subnet_b" {
+  vpc_id            = aws_vpc.main_vpc.id
+  cidr_block        = "10.0.2.0/24"
+  availability_zone = "eu-central-1b"
+}
+
+resource "aws_internet_gateway" "igw" {
+  vpc_id = aws_vpc.main_vpc.id
+}
+
+resource "aws_subnet" "public_subnet_a" {
+  vpc_id            = aws_vpc.main_vpc.id
+  cidr_block        = "10.0.3.0/24"
+  availability_zone = "eu-central-1a"
+  map_public_ip_on_launch = true
+}
+
+resource "aws_subnet" "public_subnet_b" {
+  vpc_id            = aws_vpc.main_vpc.id
+  cidr_block        = "10.0.4.0/24"
+  availability_zone = "eu-central-1b"
+  map_public_ip_on_launch = true
+}
+
+resource "aws_route_table" "public_rt" {
+  vpc_id = aws_vpc.main_vpc.id
+
+  route {
+    cidr_block = "0.0.0.0/0"
+    gateway_id = aws_internet_gateway.igw.id
+  }
+}
+
+resource "aws_route_table_association" "public_rt_assoc_a" {
+  subnet_id      = aws_subnet.public_subnet_a.id
+  route_table_id = aws_route_table.public_rt.id
+}
+
+resource "aws_route_table_association" "public_rt_assoc_b" {
+  subnet_id      = aws_subnet.public_subnet_b.id
+  route_table_id = aws_route_table.public_rt.id
 }
